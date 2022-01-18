0446873aaf6a6ef298c2e84b92d2484907c25162
Anatoliuss <achuvashlov@inbox.ru>
2022-01-18 14:30:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("У меня не сохранились предыдущие задания, а со скриншота как-то неудобно копировать... Надеюсь, так тоже можно");
+    }
+}
